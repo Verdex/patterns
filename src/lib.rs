32d635ed557a06fd528c0e2a501e6bfb772e3011
David L. Inman <davidlevere@gmail.com>
2021-08-08 21:47:28 -0400

    copy over from test project

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,31 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+#[macro_export]
+macro_rules! all_matches_no_call{
+    ($ret:ident, $next:expr, $structure:expr, $p:pat) => {
+        match $structure {
+            it @ $p => $ret.push(it),
+            _ => { },
+        }
+    };
+
+    ($ret:ident, $next:expr, $structure:expr, $p:pat, $($rest:tt)*) => {
+        match $structure {
+            it @ $p => {
+                for item in $next(it) {
+                    all_matches_no_call!($ret, $next, item, $($rest)* );
+                }
+            },
+            _ => { },
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! all_matches {
+    ($next:expr, $($rest:tt)+) => {
+        |structure| {
+            let mut ret = vec![];
+            all_matches_no_call!(ret, $next, structure, $($rest)+);
+            ret
+        }
+    };
 }
